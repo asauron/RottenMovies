49dc2cfa5d3002cd778d402f475db05b2a46f9d5
Abinaya Sarva <abinaya@fb.com>
2014-10-15 22:12:00 -0700

    Initial Commit

---
+++ RottenMovies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RottenMovies
+//
+//  Created by Abinaya Sarva on 10/15/14.
+//  Copyright (c) 2014 Sarva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
