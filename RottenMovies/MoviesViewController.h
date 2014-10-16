e4a29e2abc170e68593ed610e6f31c1ac8249dca
Abinaya Sarva <abinaya@fb.com>
2014-10-15 23:10:23 -0700

    First commit, movies table view

---
+++ RottenMovies/MoviesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MoviesViewController.h
+//  RottenMovies
+//
+//  Created by Abinaya Sarva on 10/15/14.
+//  Copyright (c) 2014 Sarva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoviesViewController : UIViewController
+
+@end
