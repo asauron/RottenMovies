b194909c4c24f13a052e22b6ac3da60127f1f467
Abinaya Sarva <abinaya@fb.com>
2014-10-18 15:33:14 -0700

    cocoa pods added

--- RottenMovies/MoviesViewController.m
+++ RottenMovies/MoviesViewController.m
@@ -8,9 +8,11 @@
 
 #import "MoviesViewController.h"
 #import "MovieCell.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface MoviesViewController ()
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic,strong) NSArray *movies;
 
 @end
 
@@ -21,18 +23,25 @@
     // Do any additional setup after loading the view from its nib.
     self.tableView.delegate = self;
     self.tableView.dataSource = self;
+    self.tableView.rowHeight = 100;
     
-    NSURL *url = [NSURL URLWithString:@"http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=29qtgez7kgf62ga9pmv22zp4"];
+    NSURL *url = [NSURL URLWithString:@"http://api.rottentomatoes.com/api/public/v1.0/lists/movies/opening.json?apikey=29qtgez7kgf62ga9pmv22zp4"];
+    
+  
     
     [self.tableView registerNib:[UINib nibWithNibName:@"MovieCell" bundle:nil] forCellReuseIdentifier:@"MovieCell"];
     
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
-    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue  mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
-        NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
-        
-        NSLog(@"response %@",responseDictionary);
-    }];
-    
+
+        [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+            
+            if(data != nil)
+            {
+                id object = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+                self.movies = object[@"movies"];
+                [self.tableView reloadData];
+            }
+        }];
     
     self.title = @"Movies List";
 }
@@ -43,14 +52,22 @@
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 10;
+    return self.movies.count;
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
     MovieCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];
-    //cell.textLabel.text = @"hello";
+    NSDictionary *movie = self.movies[indexPath.row];
+    
+    cell.titleLabel.text = movie[@"title"];
+    cell.synopsisLabel.text = movie[@"synopsis"];
+    
+    NSString *posterUrl = [movie valueForKeyPath:@"posters.thumbnail"];
+    [cell.posterView setImageWithURL:[NSURL URLWithString:posterUrl]];
+    
     return cell;
+
 }
 
 
